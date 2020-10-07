34bd156510cee0cc7da008c320dd94aae66073ef
Junekey Jeon <jk_jeon@kaist.ac.kr>
2020-10-06 22:52:43 -0700

    Fix typo

--- subproject/benchmark/source/ryu.cpp
+++ subproject/benchmark/source/ryu.cpp
@@ -27,7 +27,7 @@ namespace {
 			};
 		}
 		else {
-			static_assert(benchmark_kind == benchmark_no_trailing_zero);
+			static_assert(benchmark_kind == benchmark_allow_trailing_zero);
 			return {};
 		}
 	}();
