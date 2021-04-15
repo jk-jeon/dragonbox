7a9e6c420ec27407f0c4c46ab6baf4ce435f5954
Junekey Jeon <jk_jeon@kaist.ac.kr>
2021-04-15 11:24:27 -0700

    static constexpr globals to inline constexpr
    (no need to duplicate symbols per TU)

--- include/dragonbox/dragonbox.h
+++ include/dragonbox/dragonbox.h
@@ -574,21 +574,21 @@ namespace jkj::dragonbox {
 				return int((std::int32_t(e) * c - s) >> shift_amount);
 			}
 
-			static constexpr std::uint64_t log10_2_fractional_digits{ 0x4d10'4d42'7de7'fbcc };
-			static constexpr std::uint64_t log10_4_over_3_fractional_digits{ 0x1ffb'fc2b'bc78'0375 };
-			static constexpr std::size_t floor_log10_pow2_shift_amount = 22;
-			static constexpr int floor_log10_pow2_input_limit = 1700;
-			static constexpr int floor_log10_pow2_minus_log10_4_over_3_input_limit = 1700;
+			inline constexpr std::uint64_t log10_2_fractional_digits{ 0x4d10'4d42'7de7'fbcc };
+			inline constexpr std::uint64_t log10_4_over_3_fractional_digits{ 0x1ffb'fc2b'bc78'0375 };
+			inline constexpr std::size_t floor_log10_pow2_shift_amount = 22;
+			inline constexpr int floor_log10_pow2_input_limit = 1700;
+			inline constexpr int floor_log10_pow2_minus_log10_4_over_3_input_limit = 1700;
 
-			static constexpr std::uint64_t log2_10_fractional_digits{ 0x5269'e12f'346e'2bf9 };
-			static constexpr std::size_t floor_log2_pow10_shift_amount = 19;
-			static constexpr int floor_log2_pow10_input_limit = 1233;
+			inline constexpr std::uint64_t log2_10_fractional_digits{ 0x5269'e12f'346e'2bf9 };
+			inline constexpr std::size_t floor_log2_pow10_shift_amount = 19;
+			inline constexpr int floor_log2_pow10_input_limit = 1233;
 
-			static constexpr std::uint64_t log5_2_fractional_digits{ 0x6e40'd1a4'143d'cb94 };
-			static constexpr std::uint64_t log5_3_fractional_digits{ 0xaebf'4791'5d44'3b24 };
-			static constexpr std::size_t floor_log5_pow2_shift_amount = 20;
-			static constexpr int floor_log5_pow2_input_limit = 1492;
-			static constexpr int floor_log5_pow2_minus_log5_3_input_limit = 2427;
+			inline constexpr std::uint64_t log5_2_fractional_digits{ 0x6e40'd1a4'143d'cb94 };
+			inline constexpr std::uint64_t log5_3_fractional_digits{ 0xaebf'4791'5d44'3b24 };
+			inline constexpr std::size_t floor_log5_pow2_shift_amount = 20;
+			inline constexpr int floor_log5_pow2_input_limit = 1492;
+			inline constexpr int floor_log5_pow2_minus_log5_3_input_limit = 2427;
 
 			// For constexpr computation
 			// Returns -1 when n = 0
@@ -2230,65 +2230,65 @@ namespace jkj::dragonbox {
 	
 	namespace policy {
 		namespace sign {
-			static constexpr auto ignore = detail::policy_impl::sign::ignore{};
-			static constexpr auto return_sign = detail::policy_impl::sign::return_sign{};
+			inline constexpr auto ignore = detail::policy_impl::sign::ignore{};
+			inline constexpr auto return_sign = detail::policy_impl::sign::return_sign{};
 		}
 
 		namespace trailing_zero {
-			static constexpr auto ignore = detail::policy_impl::trailing_zero::ignore{};
-			static constexpr auto remove = detail::policy_impl::trailing_zero::remove{};
-			static constexpr auto report = detail::policy_impl::trailing_zero::report{};
+			inline constexpr auto ignore = detail::policy_impl::trailing_zero::ignore{};
+			inline constexpr auto remove = detail::policy_impl::trailing_zero::remove{};
+			inline constexpr auto report = detail::policy_impl::trailing_zero::report{};
 		}
 
 		namespace rounding_mode {
-			static constexpr auto nearest_to_even =
+			inline constexpr auto nearest_to_even =
 				detail::policy_impl::rounding_mode::nearest_to_even{};
-			static constexpr auto nearest_to_odd =
+			inline constexpr auto nearest_to_odd =
 				detail::policy_impl::rounding_mode::nearest_to_odd{};
-			static constexpr auto nearest_toward_plus_infinity =
+			inline constexpr auto nearest_toward_plus_infinity =
 				detail::policy_impl::rounding_mode::nearest_toward_plus_infinity{};
-			static constexpr auto nearest_toward_minus_infinity =
+			inline constexpr auto nearest_toward_minus_infinity =
 				detail::policy_impl::rounding_mode::nearest_toward_minus_infinity{};
-			static constexpr auto nearest_toward_zero =
+			inline constexpr auto nearest_toward_zero =
 				detail::policy_impl::rounding_mode::nearest_toward_zero{};
-			static constexpr auto nearest_away_from_zero =
+			inline constexpr auto nearest_away_from_zero =
 				detail::policy_impl::rounding_mode::nearest_away_from_zero{};
 
-			static constexpr auto nearest_to_even_static_boundary =
+			inline constexpr auto nearest_to_even_static_boundary =
 				detail::policy_impl::rounding_mode::nearest_to_even_static_boundary{};
-			static constexpr auto nearest_to_odd_static_boundary =
+			inline constexpr auto nearest_to_odd_static_boundary =
 				detail::policy_impl::rounding_mode::nearest_to_odd_static_boundary{};
-			static constexpr auto nearest_toward_plus_infinity_static_boundary =
+			inline constexpr auto nearest_toward_plus_infinity_static_boundary =
 				detail::policy_impl::rounding_mode::nearest_toward_plus_infinity_static_boundary{};
-			static constexpr auto nearest_toward_minus_infinity_static_boundary =
+			inline constexpr auto nearest_toward_minus_infinity_static_boundary =
 				detail::policy_impl::rounding_mode::nearest_toward_minus_infinity_static_boundary{};
 
-			static constexpr auto toward_plus_infinity =
+			inline constexpr auto toward_plus_infinity =
 				detail::policy_impl::rounding_mode::toward_plus_infinity{};
-			static constexpr auto toward_minus_infinity =
+			inline constexpr auto toward_minus_infinity =
 				detail::policy_impl::rounding_mode::toward_minus_infinity{};
-			static constexpr auto toward_zero =
+			inline constexpr auto toward_zero =
 				detail::policy_impl::rounding_mode::toward_zero{};
-			static constexpr auto away_from_zero =
+			inline constexpr auto away_from_zero =
 				detail::policy_impl::rounding_mode::away_from_zero{};
 		}
 
 		namespace correct_rounding {
-			static constexpr auto do_not_care = detail::policy_impl::correct_rounding::do_not_care{};
-			static constexpr auto to_even = detail::policy_impl::correct_rounding::to_even{};
-			static constexpr auto to_odd = detail::policy_impl::correct_rounding::to_odd{};
-			static constexpr auto away_from_zero = detail::policy_impl::correct_rounding::away_from_zero{};
-			static constexpr auto toward_zero = detail::policy_impl::correct_rounding::toward_zero{};
+			inline constexpr auto do_not_care = detail::policy_impl::correct_rounding::do_not_care{};
+			inline constexpr auto to_even = detail::policy_impl::correct_rounding::to_even{};
+			inline constexpr auto to_odd = detail::policy_impl::correct_rounding::to_odd{};
+			inline constexpr auto away_from_zero = detail::policy_impl::correct_rounding::away_from_zero{};
+			inline constexpr auto toward_zero = detail::policy_impl::correct_rounding::toward_zero{};
 		}
 
 		namespace cache {
-			static constexpr auto normal = detail::policy_impl::cache::normal{};
-			static constexpr auto compressed = detail::policy_impl::cache::compressed{};
+			inline constexpr auto normal = detail::policy_impl::cache::normal{};
+			inline constexpr auto compressed = detail::policy_impl::cache::compressed{};
 		}
 
 		namespace input_validation {
-			static constexpr auto assert_finite = detail::policy_impl::input_validation::assert_finite{};
-			static constexpr auto do_nothing = detail::policy_impl::input_validation::do_nothing{};
+			inline constexpr auto assert_finite = detail::policy_impl::input_validation::assert_finite{};
+			inline constexpr auto do_nothing = detail::policy_impl::input_validation::do_nothing{};
 		}
 	}
 
