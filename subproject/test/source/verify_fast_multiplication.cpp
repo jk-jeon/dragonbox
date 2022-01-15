1c1ca394fa7b584f8eabb1854759d320c4b05270
Junekey Jeon <jk_jeon@kaist.ac.kr>
2022-01-15 04:50:30 -0800

    More precise test

--- subproject/test/source/verify_fast_multiplication.cpp
+++ subproject/test/source/verify_fast_multiplication.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Junekey Jeon
+// Copyright 2020-2022 Junekey Jeon
 //
 // The contents of this file may be used under the terms of
 // the Apache License v2.0 with LLVM Exceptions.
@@ -16,52 +16,68 @@
 // KIND, either express or implied.
 
 #include "dragonbox/dragonbox.h"
+#include "big_uint.h"
+#include "continued_fractions.h"
 
 #include <iostream>
 
-template <class Float>
-static bool verify_fast_multiplication_xz() {
+template <class Float, class CachePolicy>
+static bool verify_fast_multiplication_xz(CachePolicy&& cache_policy) {
     using impl = jkj::dragonbox::detail::impl<Float>;
+    using format = typename impl::format;
     using carrier_uint = typename impl::carrier_uint;
 
-    constexpr auto fl = (carrier_uint(1) << (impl::significand_bits + 2)) - 1;
-    constexpr auto fr = (carrier_uint(1) << (impl::significand_bits + 2)) + 2;
+    constexpr auto four_fl = (carrier_uint(1) << (impl::significand_bits + 2)) - 1;
+    constexpr auto two_fr = (carrier_uint(1) << (impl::significand_bits + 1)) + 1;
 
     using jkj::dragonbox::detail::log::floor_log10_pow2_minus_log10_4_over_3;
     using jkj::dragonbox::detail::log::floor_log2_pow10;
 
     bool success = true;
 
-    for (int e = impl::min_exponent + 1; e <= impl::max_exponent; ++e) {
-        int const exponent = e - impl::significand_bits;
+    for (int e = impl::min_exponent + 1 - impl::significand_bits;
+         e <= impl::max_exponent - impl::significand_bits; ++e) {
 
-        // Compute k and beta
-        int const minus_k = floor_log10_pow2_minus_log10_4_over_3(exponent);
-        int const beta_minus_1 = exponent + floor_log2_pow10(-minus_k);
+        // Compute k and beta.
+        int const k = -floor_log10_pow2_minus_log10_4_over_3(e);
+        int const beta_minus_1 = e + floor_log2_pow10(k);
         int const beta_minus_2 = beta_minus_1 - 1;
 
-        // Load cache
-        auto const cache =
-            jkj::dragonbox::policy::cache::full.get_cache<typename impl::format>(-minus_k);
+        // Load cache.
+        auto const cache = cache_policy.get_cache<format>(k);
 
-        // Compute the endpoints using the fast method
+        // Compute the endpoints using the fast method.
         auto x_fast = impl::compute_left_endpoint_for_shorter_interval_case(cache, beta_minus_1);
         auto z_fast = impl::compute_right_endpoint_for_shorter_interval_case(cache, beta_minus_1);
 
-        // Compute the endpoints using the exact method
-        auto x_exact = beta_minus_2 >= 0 ? impl::compute_mul(fl << beta_minus_2, cache).result
-                                         : impl::compute_mul(fl, cache).result >> -beta_minus_2;
-        auto z_exact = beta_minus_2 >= 0 ? impl::compute_mul(fr << beta_minus_2, cache).result
-                                         : impl::compute_mul(fr, cache).result >> -beta_minus_2;
+        // Precisely compute the endpoints.
+        jkj::unsigned_rational<jkj::big_uint> precise_multiplier{1, 1};
+        if (k >= 0) {
+            precise_multiplier.numerator = jkj::big_uint::pow(5, k);
+        }
+        else {
+            precise_multiplier.denominator = jkj::big_uint::pow(5, -k);
+        }
+        if (e + k >= 0) {
+            precise_multiplier.numerator *= jkj::big_uint::power_of_2(e + k);
+        }
+        else {
+            precise_multiplier.denominator *= jkj::big_uint::power_of_2(-e - k);
+        }
 
-        if (x_fast != x_exact) {
+        auto x_exact =
+            (four_fl * precise_multiplier.numerator) / (4 * precise_multiplier.denominator);
+        auto z_exact =
+            (two_fr * precise_multiplier.numerator) / (2 * precise_multiplier.denominator);
+
+        if (x_exact != x_fast) {
             std::cout << "(e = " << e << ") left endpoint is not correct; computed = " << x_fast
-                      << "; true_value = " << x_exact << "\n";
+                      << "; true_value = " << x_exact[0] << "\n";
             success = false;
         }
-        if (z_fast != z_exact) {
+        if (z_exact != z_fast) {
             std::cout << "(e = " << e << ") right endpoint is not correct; computed = " << z_fast
-                      << "; true_value = " << z_exact << "\n";
+                      << "; true_value = " << z_exact[0] << "\n";
             success = false;
         }
     }
@@ -76,15 +92,15 @@ static bool verify_fast_multiplication_xz() {
     return success;
 }
 
-template <class Float>
-static bool verify_fast_multiplication_yru() {
+template <class Float, class CachePolicy>
+static bool verify_fast_multiplication_yru(CachePolicy&& cache_policy) {
     using impl = jkj::dragonbox::detail::impl<Float>;
     bool success = true;
 
-    for (int k = impl::min_k; k < 0; ++k) {
-        auto const cache = jkj::dragonbox::policy::cache::full.get_cache<typename impl::format>(k);
+    for (int k = impl::min_k; k <= impl::max_k; ++k) {
+        auto const cache = cache_policy.get_cache<typename impl::format>(k);
 
-        // Since p + beta <= q, suffices to check that the lower half of the cache is not 0
+        // Since p + beta + 1 <= q, suffices to check that the lower half of the cache is not 0
         auto const lower_half = [cache] {
             if constexpr (std::is_same_v<typename impl::format, jkj::dragonbox::ieee754_binary32>) {
                 return std::uint32_t(cache);
@@ -94,9 +110,12 @@ static bool verify_fast_multiplication_yru() {
             }
         }();
 
+        // If the lower half is zero, we need to check if the cache is precise.
         if (lower_half == 0) {
-            std::cout << "(k = " << k << ") computation might be incorrect\n";
-            success = false;
+            if (k < 0 || k > jkj::dragonbox::detail::log::floor_log5_pow2(impl::cache_bits)) {
+                std::cout << "(k = " << k << ") computation might be incorrect\n";
+                success = false;
+            }
         }
     }
 
@@ -113,22 +132,29 @@ static bool verify_fast_multiplication_yru() {
 int main() {
     bool success = true;
 
-    std::cout
-        << "[Verifying fast computation of xi and zi for the closer boundary case (binary32)...]\n";
-    success &= verify_fast_multiplication_xz<float>();
+    std::cout << "[Verifying fast computation of xi and zi for the shorter interval case "
+                 "with full cache (binary32)...]\n";
+    success &= verify_fast_multiplication_xz<float>(jkj::dragonbox::policy::cache::full);
     std::cout << "Done.\n\n\n";
 
-    std::cout << "[Verifying fast computation of yru for the closer boundary case (binary32)...]\n";
-    success &= verify_fast_multiplication_yru<float>();
+    std::cout << "[Verifying fast computation of yru for the shorter interval case "
+                 "with full cache (binary32)...]\n";
+    success &= verify_fast_multiplication_yru<float>(jkj::dragonbox::policy::cache::full);
     std::cout << "Done.\n\n\n";
 
-    std::cout
-        << "[Verifying fast computation of xi and zi for the closer boundary case (binary64)...]\n";
-    success &= verify_fast_multiplication_xz<double>();
+    std::cout << "[Verifying fast computation of xi and zi for the shorter interval case "
+                 "with full cache (binary64)...]\n";
+    success &= verify_fast_multiplication_xz<double>(jkj::dragonbox::policy::cache::full);
     std::cout << "Done.\n\n\n";
 
-    std::cout << "[Verifying fast computation of yru for the closer boundary case (binary64)...]\n";
-    success &= verify_fast_multiplication_yru<double>();
+    std::cout << "[Verifying fast computation of xi and zi for the shorter interval case "
+                 "with compressed cache (binary64)...]\n";
+    success &= verify_fast_multiplication_xz<double>(jkj::dragonbox::policy::cache::compact);
+    std::cout << "Done.\n\n\n";
+
+    std::cout << "[Verifying fast computation of yru for the shorter interval case "
+                 "with full cache (binary64)...]\n";
+    success &= verify_fast_multiplication_yru<double>(jkj::dragonbox::policy::cache::full);
     std::cout << "Done.\n\n\n";
 
     if (!success) {
