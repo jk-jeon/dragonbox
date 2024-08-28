207fd2307ddd57ee59450d8769134bd1d2566e58
jk-jeon <j6jeon@ucsd.edu>
2024-08-28 14:03:53 -0700

    Add proper copyright notice

--- subproject/simple/simple_dragonbox.h
+++ subproject/simple/simple_dragonbox.h
@@ -1,8 +1,23 @@
+// Copyright 2024 Junekey Jeon, Toby Bell
+// 
+// The contents of this file may be used under the terms of
+// the Apache License v2.0 with LLVM Exceptions.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     https://llvm.org/foundation/relicensing/LICENSE.txt)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+
+
 #ifndef JKJ_HEADER_SIMPLE_DRAGONBOX
 #define JKJ_HEADER_SIMPLE_DRAGONBOX
-
-// Copyright 2024 Junekey Jeon, Toby Bell
-// TODO: License
 
 #include <cassert>
 #include <climits>
--- subproject/simple/simple_dragonbox_test.cpp
+++ subproject/simple/simple_dragonbox_test.cpp
@@ -1,5 +1,19 @@
 // Copyright 2024 Junekey Jeon, Toby Bell
-// TODO: License
+//
+// The contents of this file may be used under the terms of
+// the Apache License v2.0 with LLVM Exceptions.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     https://llvm.org/foundation/relicensing/LICENSE.txt)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
 
 #include "simple_dragonbox.h"
 #include "random_float.h"
