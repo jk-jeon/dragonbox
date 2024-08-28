90568ea11c9b77fedffea7bd1cc1793c0c9c70b2
jk-jeon <j6jeon@ucsd.edu>
2024-08-28 14:05:59 -0700

    Wrap simple_dragonbox inside jkj namespace and fix some warnings

--- subproject/simple/simple_dragonbox_test.cpp
+++ subproject/simple/simple_dragonbox_test.cpp
@@ -32,8 +32,8 @@ static bool test_simple_dragonbox(bool& success, Arg... arg) {
     static constexpr char const* compact = sizeof...(arg) ? "yes" : "no";
     static constexpr unsigned test_count = 10000000;
 
-    std::cout << "uniform random test, type=" << type_name << " test_count=" << test_count << 
-    " compact_cache=" << compact << " ...\n";
+    std::cout << "uniform random test, type=" << type_name << " test_count=" << test_count
+              << " compact_cache=" << compact << " ...\n";
 
     char buffer1[64];
     char buffer2[64];
@@ -42,7 +42,7 @@ static bool test_simple_dragonbox(bool& success, Arg... arg) {
         auto x = uniformly_randomly_generate_general_float<Float>(rg);
 
         // Check if the output is identical to the reference implementation (Ryu).
-        simple_dragonbox::to_chars(x, buffer1, arg...);
+        jkj::simple_dragonbox::to_chars(x, buffer1, arg...);
         reference_implementation(x, buffer2);
 
         std::string_view view1(buffer1);
@@ -56,21 +56,21 @@ static bool test_simple_dragonbox(bool& success, Arg... arg) {
 
     std::cout << "done.\n";
 
-    std::cout << "test all shorter interval cases, type=" << type_name << " compact_cache=" << compact << " ... \n";
+    std::cout << "test all shorter interval cases, type=" << type_name << " compact_cache=" << compact
+              << " ... \n";
 
-    using format = simple_dragonbox::detail::float_format<Float>;
+    using format = jkj::simple_dragonbox::detail::float_format<Float>;
     using carrier_uint = typename format::carrier_uint;
 
     for (int e = format::min_exponent; e <= format::max_exponent; ++e) {
         // Compose a floating-point number
-        carrier_uint br = carrier_uint(e - format::exponent_bias)
-                          << format::significand_bits;
+        carrier_uint br = carrier_uint(e - format::exponent_bias) << format::significand_bits;
 
         Float x;
         static_assert(sizeof(br) == sizeof(x));
         std::memcpy(&x, &br, sizeof(br));
 
-        simple_dragonbox::to_chars(x, buffer1, arg...);
+        jkj::simple_dragonbox::to_chars(x, buffer1, arg...);
         reference_implementation(x, buffer2);
 
         std::string_view view1(buffer1);
@@ -90,10 +90,9 @@ static bool test_simple_dragonbox(bool& success, Arg... arg) {
 int main() {
     bool success = true;
     test_simple_dragonbox<float>(success);
-    test_simple_dragonbox<float>(success, simple_dragonbox::policy::cache::compact);
+    test_simple_dragonbox<float>(success, jkj::simple_dragonbox::policy::cache::compact);
     test_simple_dragonbox<double>(success);
-    test_simple_dragonbox<double>(success, simple_dragonbox::policy::cache::compact);
+    test_simple_dragonbox<double>(success, jkj::simple_dragonbox::policy::cache::compact);
     if (!success)
         return -1;
 }
-
